a24d96cca7995f7339ad7c78db31760eda85f645
paulluger <32573579+paulluger@users.noreply.github.com>
2017-10-06 16:19:45 +0200

    Update Programm (see #1)

--- Programm.cs
+++ Programm.cs
@@ -2,7 +2,7 @@ public static class Programm
 {
     public static void Main(string[] args)
     {
-        System.Console.WriteLine("Hello  4AHWII\n");
+        System.Console.WriteLine("Hello  4ahwii\n");
     }
 
 }
